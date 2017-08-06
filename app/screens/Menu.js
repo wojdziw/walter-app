7e1ab26f6bdd294e69d4ca2444aea1f046cd2454
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-06 22:28:40 +0200

    Menu working

--- app/screens/Menu.js
+++ app/screens/Menu.js
@@ -1,45 +1,84 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-} from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { Header, Buttons, Total, Activity, MenuTile } from '../components'
+import { bindActionCreators } from 'redux'
+import { ActionCreators } from '../actions'
+import { connect } from 'react-redux'
+import colors from '../static/colors'
 
-export default class Menu extends Component {
+class Menu extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {menuItemsLength: 0}
+  }
+
+  componentWillMount() {
+    this.props.fetchMenu();
+  }
+
+  componentDidMount() {
+    refreshPositions = setInterval(() => 
+      {this.setState({menuPositionsLength: Object.keys(this.props.order.items).length}); 
+      if(this.state.menuItemsLength>0) {
+        clearInterval(refreshPositions);
+      }
+      }, 500);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(refreshPositions);
+  }
+
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Wojtek jest super!
-        </Text>
-        <Text style={styles.instructions}>
-          Ooo tak, wlasnie tak
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Header />
+
+        {(Object.keys(this.props.order.items).length > 0) && <MenuTiles {...this.props}/>}
+
+        {(Object.keys(this.props.order.items).length == 0) && <Activity />} 
+
+        <Total {...this.props} />
+        
+        <Buttons
+        prevName = "table"
+        nextName = "summary"
+        displayPrev={true}
+        displayNext={true}
+        onPressPrev={() => {console.log('Tables')}}
+        onPressNext={() => {console.log('Summary')}}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class MenuTiles extends Component {
+  render() {
+    return (
+      <ScrollView style={{paddingTop: 4, paddingLeft: 4, paddingRight: 4 , backgroundColor: colors.background }}>
+        {Object.values(this.props.order.items).map((position, index) => {
+          return (
+            position.id != null &&
+            <View key={position.id} style={{height: 250, flexDirection: 'row'}}>
+              <MenuTile key={position.id} id={position.id} {...this.props}/>
+            </View>
+          )
+        })}
+      </ScrollView>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(ActionCreators, dispatch);
+}
+function mapStateToProps(state) {
+  return {
+    order: state.order
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
